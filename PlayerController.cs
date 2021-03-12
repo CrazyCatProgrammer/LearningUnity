3cbfc74cedee546919105651c21bdf1323546feb
CrazyCatProgrammer <meowcollins@gmail.com>
2021-03-12 13:06:29 -0800

    Added load level with start position
    fixes creating multiple player bugs.

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject[] players;
     public bool facingRight = true; //Depends on if your animation is by default facing right or left
     Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
+    //Vector2 lookDirection = new Vector2(1, 0);
     public bool Walk;
 
-    float horizontal;
+    //float horizontal;
 
     public float speed;
     public float jumpForce;
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        DontDestroyOnLoad(gameObject);
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         Walk = false;
@@ -28,8 +30,9 @@ public class PlayerController : MonoBehaviour
 
         float move = Input.GetAxis("Horizontal");
 
+        //look up checking if key is down
         //determining walk animation and setting walk to true or false.
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
             animator.SetBool("Walk", true);
         }
@@ -51,11 +54,12 @@ public class PlayerController : MonoBehaviour
     }
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if(Input.GetKey(KeyCode.Space) && !isJumping)
         {
             animator.SetBool("isJumping", true);
 
             isJumping = true;
+            Walk = false;
 
             rigidbody2d.AddForce(new Vector2(rigidbody2d.velocity.x, jumpForce));
         }
@@ -71,6 +75,7 @@ public class PlayerController : MonoBehaviour
             rigidbody2d.velocity = Vector2.zero;
         }
     }
+
     // flips the character to the direction they are moving. 
     void Flip()
     {
@@ -79,4 +84,24 @@ public class PlayerController : MonoBehaviour
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+    // makes sure only ONE player loads on each level
+    private void OnLevelWasLoaded(int level)
+    {
+        FindStartPos();
+
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        if(players.Length > 1)
+        {
+            Destroy(players[1]);
+        }
+    }
+
+    //find start position and put player there.
+    void FindStartPos()
+    {
+        transform.position = GameObject.FindWithTag("StartPos").transform.position;
+    }
 }
+
+
